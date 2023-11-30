590cf150f7949696d62ed630cf230bcbe861bca3
goga-m <goga.mamoulasvili@gmail.com>
2023-11-30 14:50:24 +0100

    wip

--- packages/ark/webpack.config.cjs
+++ packages/ark/webpack.config.cjs
@@ -3,19 +3,30 @@ const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
 
 module.exports = {
 	mode: "production",
-	// entry: {
-	// 	"transfer.js": ["./source/crypto/validation/validators/source/transfer.js"],
-	// },
+	entry: "./source/crypto/validation/validators/source/transfer.js",
 	output: {
-		filename: "[name]",
+		filename: "transfer.js",
 		path: path.resolve(__dirname, "./source/crypto/validation/validators/source/"),
 	},
-	target: "node",
-	plugins: [
-		new MergeIntoSingleFilePlugin({
-			files: {
-				"transfer.js": ["./source/crypto/validation/validators/source/transfer.js"],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
 			},
-		}),
-	],
+		],
+	},
+	// plugins: [
+	// 	new MergeIntoSingleFilePlugin({
+	// 		files: {
+	// 			"transfer.js": ["./source/crypto/validation/validators/source/transfer.js"],
+	// 		},
+	// 	}),
+	// ],
 };
