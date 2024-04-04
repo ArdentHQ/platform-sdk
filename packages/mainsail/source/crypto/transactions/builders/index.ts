d647b81eba339af6c76262263311f02666284380
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-04-04 10:14:34 -0600

    remove second signature transaction builder and registration

--- packages/mainsail/source/crypto/transactions/builders/index.ts
+++ packages/mainsail/source/crypto/transactions/builders/index.ts
@@ -3,29 +3,8 @@ import { DelegateResignationBuilder } from "./delegate-resignation.js";
 import { IPFSBuilder } from "./ipfs.js";
 import { MultiPaymentBuilder } from "./multi-payment.js";
 import { MultiSignatureBuilder } from "./multi-signature.js";
-import { SecondSignatureBuilder } from "./second-signature.js";
-import { Application } from "@mainsail/kernel";
-import { Container } from "@mainsail/container";
-import {
-	ServiceProvider as CoreCryptoTransactionTransfer,
-	TransferBuilder,
-} from "@mainsail/crypto-transaction-transfer";
-import { ServiceProvider as CoreCryptoTransactionVote, VoteBuilder } from "@mainsail/crypto-transaction-vote";
-import { Identifiers } from "@mainsail/contracts";
-
-import { milestones } from "../../networks/devnet/milestones";
-import { network } from "../../networks/devnet/network";
-import { ServiceProvider as CoreValidation } from "@mainsail/validation";
-import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config/distribution/service-provider";
-import { ServiceProvider as CoreCryptoValidation } from "@mainsail/crypto-validation/distribution/service-provider";
-import { ServiceProvider as CoreCryptoKeyPairEcdsa } from "@mainsail/crypto-key-pair-ecdsa";
-import { ServiceProvider as CoreCryptoAddressBase58 } from "@mainsail/crypto-address-base58";
-import { ServiceProvider as CoreCryptoSignatureSchnorr } from "@mainsail/crypto-signature-schnorr-secp256k1";
-import { ServiceProvider as CoreCryptoHashBcrypto } from "@mainsail/crypto-hash-bcrypto";
-import { ServiceProvider as CoreFees } from "@mainsail/fees/distribution/service-provider";
-import { ServiceProvider as CoreFeesStatic } from "@mainsail/fees-static";
-import { ServiceProvider as CoreCryptoTransaction } from "@mainsail/crypto-transaction/distribution/service-provider";
-import { ServiceProvider as CoreCryptoMultipaymentTransfer } from "@mainsail/crypto-transaction-multi-payment";
+import { TransferBuilder } from "./transfer.js";
+import { VoteBuilder } from "./vote.js";
 
 export * from "./transaction.js";
 
@@ -58,10 +37,6 @@ export class BuilderFactory {
 	public static async transfer(): Promise<TransferBuilder> {
 		const app = await this.app();
 		return app.resolve(TransferBuilder);
-	}
-
-	public static secondSignature(): SecondSignatureBuilder {
-		return new SecondSignatureBuilder();
 	}
 
 	public static delegateRegistration(): DelegateRegistrationBuilder {
