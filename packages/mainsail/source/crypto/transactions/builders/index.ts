c6a250d7eaba1cfa2d8df2fa7ab97ee84b885043
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-04-04 10:21:39 -0600

    Update index.ts

--- packages/mainsail/source/crypto/transactions/builders/index.ts
+++ packages/mainsail/source/crypto/transactions/builders/index.ts
@@ -1,30 +1,30 @@
-import { Container } from "@mainsail/container";
-import { Identifiers } from "@mainsail/contracts";
-import { ServiceProvider as CoreCryptoAddressBase58 } from "@mainsail/crypto-address-base58";
-import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config/distribution/service-provider";
-import { ServiceProvider as CoreCryptoHashBcrypto } from "@mainsail/crypto-hash-bcrypto";
-import { ServiceProvider as CoreCryptoKeyPairEcdsa } from "@mainsail/crypto-key-pair-ecdsa";
-import { ServiceProvider as CoreCryptoSignatureSchnorr } from "@mainsail/crypto-signature-schnorr-secp256k1";
-import { ServiceProvider as CoreCryptoTransaction } from "@mainsail/crypto-transaction/distribution/service-provider";
-import { ServiceProvider as CoreCryptoMultipaymentTransfer } from "@mainsail/crypto-transaction-multi-payment";
-import {
-	ServiceProvider as CoreCryptoTransactionTransfer,
-	TransferBuilder,
-} from "@mainsail/crypto-transaction-transfer";
-import { ServiceProvider as CoreCryptoTransactionVote, VoteBuilder } from "@mainsail/crypto-transaction-vote";
-import { ServiceProvider as CoreCryptoValidation } from "@mainsail/crypto-validation/distribution/service-provider";
-import { ServiceProvider as CoreFees } from "@mainsail/fees/distribution/service-provider";
-import { ServiceProvider as CoreFeesStatic } from "@mainsail/fees-static";
-import { Application } from "@mainsail/kernel";
-import { ServiceProvider as CoreValidation } from "@mainsail/validation";
-
-import { milestones } from "../../networks/devnet/milestones";
-import { network } from "../../networks/devnet/network";
 import { DelegateRegistrationBuilder } from "./delegate-registration.js";
 import { DelegateResignationBuilder } from "./delegate-resignation.js";
 import { IPFSBuilder } from "./ipfs.js";
 import { MultiPaymentBuilder } from "./multi-payment.js";
 import { MultiSignatureBuilder } from "./multi-signature.js";
+import { Application } from "@mainsail/kernel";
+import { Container } from "@mainsail/container";
+import {
+	ServiceProvider as CoreCryptoTransactionTransfer,
+	TransferBuilder,
+} from "@mainsail/crypto-transaction-transfer";
+import { ServiceProvider as CoreCryptoTransactionVote, VoteBuilder } from "@mainsail/crypto-transaction-vote";
+import { Identifiers } from "@mainsail/contracts";
+
+import { milestones } from "../../networks/devnet/milestones";
+import { network } from "../../networks/devnet/network";
+import { ServiceProvider as CoreValidation } from "@mainsail/validation";
+import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config/distribution/service-provider";
+import { ServiceProvider as CoreCryptoValidation } from "@mainsail/crypto-validation/distribution/service-provider";
+import { ServiceProvider as CoreCryptoKeyPairEcdsa } from "@mainsail/crypto-key-pair-ecdsa";
+import { ServiceProvider as CoreCryptoAddressBase58 } from "@mainsail/crypto-address-base58";
+import { ServiceProvider as CoreCryptoSignatureSchnorr } from "@mainsail/crypto-signature-schnorr-secp256k1";
+import { ServiceProvider as CoreCryptoHashBcrypto } from "@mainsail/crypto-hash-bcrypto";
+import { ServiceProvider as CoreFees } from "@mainsail/fees/distribution/service-provider";
+import { ServiceProvider as CoreFeesStatic } from "@mainsail/fees-static";
+import { ServiceProvider as CoreCryptoTransaction } from "@mainsail/crypto-transaction/distribution/service-provider";
+import { ServiceProvider as CoreCryptoMultipaymentTransfer } from "@mainsail/crypto-transaction-multi-payment";
 
 export * from "./transaction.js";
 
