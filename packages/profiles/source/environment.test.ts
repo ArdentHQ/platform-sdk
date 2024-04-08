ee199c7fdfd31cc4cde86f7177b0f31223b6ef86
Shahin Safaraliyev <shahin@ardenthq.com>
2024-04-08 17:13:18 +0400

    fix tests

--- packages/profiles/source/environment.test.ts
+++ packages/profiles/source/environment.test.ts
@@ -341,7 +341,7 @@ describe("Environment", ({ beforeEach, it, assert, nock, loader }) => {
 
 		await context.subject.fees().sync(await context.subject.profiles().create("John"), "ARK", "ark.devnet");
 
-		assert.length(Object.keys(context.subject.fees().all("ARK", "ark.devnet")), 9);
+		assert.length(Object.keys(context.subject.fees().all("ARK", "ark.devnet")), 10);
 	});
 
 	it("#delegates", async (context) => {
