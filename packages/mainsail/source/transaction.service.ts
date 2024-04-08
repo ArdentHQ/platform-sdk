3ad5b81a52d41a4bf5927e842722a2ce0d48b68f
Shahin Safaraliyev <shahin@ardenthq.com>
2024-04-08 17:26:11 +0400

    wip

--- packages/mainsail/source/transaction.service.ts
+++ packages/mainsail/source/transaction.service.ts
@@ -18,7 +18,7 @@ import { ServiceProvider as CoreCryptoHashBcrypto } from "@mainsail/crypto-hash-
 import { ServiceProvider as CoreFees } from "@mainsail/fees";
 import { ServiceProvider as CoreFeesStatic } from "@mainsail/fees-static";
 import { ServiceProvider as CoreCryptoTransaction } from "@mainsail/crypto-transaction";
-import { ServiceProvider as CoreCryptoTransactionTransfer } from "@mainsail/crypto-transaction-transfer";
+import { ServiceProvider as CoreCryptoTransactionTransfer, TransferBuilder } from "@mainsail/crypto-transaction-transfer";
 import { Container } from "@mainsail/container";
 
 import {
