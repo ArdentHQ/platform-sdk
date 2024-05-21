7f6291dbe67391dd1afafafa0294f7aa345fd495
goga-m <goga.mamoulasvili@gmail.com>
2024-05-21 18:13:20 +0200

    wip

--- packages/mainsail/source/public-key.service.ts
+++ packages/mainsail/source/public-key.service.ts
@@ -1,41 +1,22 @@
 import { IoC, Services } from "@ardenthq/sdk";
 import { BIP39 } from "@ardenthq/sdk-cryptography";
 import { abort_if, abort_unless } from "@ardenthq/sdk-helpers";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Application } from "@mainsail/kernel";
 
 import { BindingType } from "./coin.contract.js";
 import { PublicKey as BasePublicKey } from "./crypto/identities/public-key.js";
 import { Interfaces } from "./crypto/index.js";
-import { Container } from "@mainsail/container";
-import { Application } from "@mainsail/kernel";
-import { Contracts } from "@mainsail/contracts";
-import { ServiceProvider as CoreValidation } from "@mainsail/validation";
-import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config";
-import { ServiceProvider as CoreCryptoConsensusBls12381 } from "@mainsail/crypto-consensus-bls12-381";
-import { ServiceProvider as CoreCryptoValidation } from "@mainsail/crypto-validation";
-import { Identifiers } from "@mainsail/contracts";
 
 export class PublicKeyService extends Services.AbstractPublicKeyService {
 	readonly #config!: Interfaces.NetworkConfig;
 	readonly #app: Application;
-	#isBooted: boolean = false;
 
 	public constructor(container: IoC.IContainer) {
 		super(container);
 
 		this.#config = container.get(BindingType.Crypto);
-
-		this.#app = new Application(new Container());
-
-		this.#boot();
-	}
-
-	async #boot(): Promise<void> {
-		await this.#app.resolve(CoreValidation).register();
-		await this.#app.resolve(CoreCryptoConfig).register();
-		await this.#app.resolve(CoreCryptoValidation).register();
-		await this.#app.resolve(CoreCryptoConsensusBls12381).register();
-
-		this.#isBooted = true;
+		this.#app = container.resolve<any>(container.get(BindingType.ServiceProvider)).app();
 	}
 
 	public override async fromMnemonic(
@@ -73,10 +54,6 @@ export class PublicKeyService extends Services.AbstractPublicKeyService {
 	}
 
 	public override async verifyPublicKeyWithBLS(publicKey: string): Promise<boolean> {
-		if (!this.#isBooted) {
-			await this.#boot();
-		}
-
 		const consensusKeyPairFactory: Contracts.Crypto.KeyPairFactory = this.#app.getTagged(
 			Identifiers.Cryptography.Identity.KeyPair.Factory,
 			"type",
