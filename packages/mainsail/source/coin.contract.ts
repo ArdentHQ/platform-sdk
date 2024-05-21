f1084e8534cb1c60b6bbe282e4eed9a652099259
goga-m <goga.mamoulasvili@gmail.com>
2024-05-21 18:13:12 +0200

    wip

--- packages/mainsail/source/coin.contract.ts
+++ packages/mainsail/source/coin.contract.ts
@@ -1,3 +1,5 @@
+import { ServiceProvider } from "@mainsail/crypto-address-base58";
+
 export interface CryptoConfig {
 	pubKeyHash: number;
 	wif: number;
@@ -7,4 +9,5 @@ export const BindingType = {
 	Crypto: Symbol.for("ARK<Crypto>"),
 	Height: Symbol.for("ARK<Height>"),
 	MultiSignatureSigner: Symbol.for("ARK<MultiSignatureSigner>"),
+	ServiceProvider: Symbol.for("Coin<ServiceProvider>"),
 };
