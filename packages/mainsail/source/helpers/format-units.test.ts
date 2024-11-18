f53e146cb6a64b5a3f50120f6338b8a05f1fccce
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-18 10:35:30 -0500

    fix: unit tests

--- packages/mainsail/source/helpers/format-units.test.ts
+++ packages/mainsail/source/helpers/format-units.test.ts
@@ -4,7 +4,7 @@ describe("formatUnits", async ({ assert, it, loader }) => {
     it("should format the value to wei", () => {
         assert.equal(formatUnits("1", "wei"), 1);
         assert.equal(formatUnits("1000000000", "gwei"), 1);
-        assert.equal(formatUnits("100000000", "ark"), 1);
+        assert.equal(formatUnits("100000000000000000000", "ark"), 1);
     });
 
     it("should throw an error for unsupported units", () => {
