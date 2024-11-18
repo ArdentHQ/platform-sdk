650b7cd1b90cf3df1d967203fc1840a5294791b6
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-18 10:48:30 -0500

    fix: use big number for methods

--- packages/mainsail/source/helpers/format-units.ts
+++ packages/mainsail/source/helpers/format-units.ts
@@ -1,13 +1,16 @@
+/* eslint-disable unicorn/require-number-to-fixed-digits-argument */
+import { BigNumber } from "@mainsail/kernel/distribution/utils";
+
 import { ARK_MULTIPLIER, GWEI_MULTIPLIER, WEI_MULTIPLIER } from "../crypto/constants";
 
-export const formatUnits = (value: string, unit = "ark"): number => {
+export const formatUnits = (value: string, unit = "ark"): string => {
 	switch (unit.toLowerCase()) {
 		case "wei":
-			return Number(value) / WEI_MULTIPLIER;
+            return BigNumber.make(value).dividedBy(WEI_MULTIPLIER).toFixed();
 		case "gwei":
-			return Number(value) / GWEI_MULTIPLIER;
-		case "ark":
-			return Number(value) / ARK_MULTIPLIER;
+            return BigNumber.make(value).dividedBy(GWEI_MULTIPLIER).toFixed();
+        case "ark":
+            return BigNumber.make(value).dividedBy(ARK_MULTIPLIER).toFixed();
 		default:
 			throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
 	}
