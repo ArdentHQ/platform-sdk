c58e8b95ca2795e3f7ffd01a4a18095cc4c6fd7f
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-11-18 15:51:34 +0000

    style: resolve style guide violations

--- packages/mainsail/source/helpers/format-units.test.ts
+++ packages/mainsail/source/helpers/format-units.test.ts
@@ -1,11 +1,11 @@
 import { formatUnits } from "./format-units";
 
 describe("formatUnits", async ({ assert, it, loader }) => {
-    it("should format the value to wei", () => {
-        assert.equal(formatUnits("1", "wei"), "1");
-        assert.equal(formatUnits("1000000000", "gwei"), "1");
-        assert.equal(formatUnits("100000000000000000000", "ark"), "1");
-    });
+	it("should format the value to wei", () => {
+		assert.equal(formatUnits("1", "wei"), "1");
+		assert.equal(formatUnits("1000000000", "gwei"), "1");
+		assert.equal(formatUnits("100000000000000000000", "ark"), "1");
+	});
 
 	it("should throw an error for unsupported units", () => {
 		assert.throws(
