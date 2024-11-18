5ce9dfae9ef74f71c20f74cb35b96b5ef2e021fd
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-18 09:24:00 -0500

    feat: parse units util

---
+++ packages/helpers/source/parse-units.ts
@@ -0,0 +1,14 @@
+import { ARK_MULTIPLIER, GWEI_MULTIPLIER, WEI_MULTIPLIER } from '../../mainsail/source/crypto/constants';
+
+export const parseUnits = (value: number | string, unit = 'ark'): string => {
+    switch (unit.toLowerCase()) {
+        case 'wei':
+            return (Number(value) * WEI_MULTIPLIER).toString();
+        case 'gwei':
+            return (Number(value) * GWEI_MULTIPLIER).toString();
+        case 'ark':
+            return (Number(value) * ARK_MULTIPLIER).toString();
+        default:
+            throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
+    }
+}
