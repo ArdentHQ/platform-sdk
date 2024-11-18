e78e457962f2dbd319f799d80429fd06e90fd26d
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-11-18 15:04:58 +0000

    style: resolve style guide violations

--- packages/mainsail/source/helpers/parse-units.ts
+++ packages/mainsail/source/helpers/parse-units.ts
@@ -1,14 +1,14 @@
 import { ARK_MULTIPLIER, GWEI_MULTIPLIER, WEI_MULTIPLIER } from "../crypto/constants";
 
-export const parseUnits = (value: number | string, unit = 'ark'): string => {
-    switch (unit.toLowerCase()) {
-        case 'wei':
-            return (Number(value) * WEI_MULTIPLIER).toString();
-        case 'gwei':
-            return (Number(value) * GWEI_MULTIPLIER).toString();
-        case 'ark':
-            return (Number(value) * ARK_MULTIPLIER).toString();
-        default:
-            throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
-    }
-}
+export const parseUnits = (value: number | string, unit = "ark"): string => {
+	switch (unit.toLowerCase()) {
+		case "wei":
+			return (Number(value) * WEI_MULTIPLIER).toString();
+		case "gwei":
+			return (Number(value) * GWEI_MULTIPLIER).toString();
+		case "ark":
+			return (Number(value) * ARK_MULTIPLIER).toString();
+		default:
+			throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
+	}
+};
