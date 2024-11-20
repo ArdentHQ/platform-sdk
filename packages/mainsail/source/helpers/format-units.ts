1a76f8a44a0892a89e4f95e946b73cdc8c00340f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-20 09:27:54 -0500

    wip

--- packages/mainsail/source/helpers/format-units.ts
+++ packages/mainsail/source/helpers/format-units.ts
@@ -5,11 +5,11 @@ import { ARK_MULTIPLIER, GWEI_MULTIPLIER, WEI_MULTIPLIER } from "../crypto/const
 export const formatUnits = (value: string, unit = "ark"): string => {
 	switch (unit.toLowerCase()) {
 		case "wei":
-			return BigNumber.make(value).dividedBy(WEI_MULTIPLIER).valueOf();
+			return BigNumber.make(value).divide(WEI_MULTIPLIER).valueOf();
 		case "gwei":
-			return BigNumber.make(value).dividedBy(GWEI_MULTIPLIER).valueOf();
+			return BigNumber.make(value).divide(GWEI_MULTIPLIER).valueOf();
 		case "ark":
-			return BigNumber.make(value).dividedBy(ARK_MULTIPLIER).valueOf();
+			return BigNumber.make(value).divide(ARK_MULTIPLIER).valueOf();
 		default:
 			throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
 	}
