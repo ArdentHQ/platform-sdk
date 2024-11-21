14753e35a0b1a3c2a5968ec0c456e5cc117f164c
goga-m <goga.mamoulasvili@gmail.com>
2024-11-21 11:38:54 +0100

    wip

--- packages/mainsail/source/transaction.service.ts
+++ packages/mainsail/source/transaction.service.ts
@@ -1,23 +1,22 @@
-import { Contracts, IoC, Services, Signatories } from "@ardenthq/sdk";
-import { BigNumber } from "@ardenthq/sdk-helpers";
-import { Contracts as MainsailContracts } from "@mainsail/contracts";
-import { TransactionBuilder, Utils } from "@mainsail/crypto-transaction";
+import { Contracts, IoC, Services } from "@ardenthq/sdk";
+import { Exceptions } from "@mainsail/contracts";
 import { EvmCallBuilder } from "@mainsail/crypto-transaction-evm-call";
 import { Application } from "@mainsail/kernel";
 
 import { BindingType } from "./coin.contract.js";
 import { applyCryptoConfiguration } from "./config.js";
-import { Identities, Interfaces, Transactions } from "./crypto/index.js";
+import { Interfaces, Transactions } from "./crypto/index.js";
 import { BuilderFactory } from "./crypto/transactions/index.js";
-import { MultiSignatureSigner } from "./multi-signature.signer.js";
 import { Request } from "./request.js";
+
+export enum GasLimit {
+	Transfer = 21_000
+}
 
 export class TransactionService extends Services.AbstractTransactionService {
 	readonly #ledgerService!: Services.LedgerService;
 	readonly #addressService!: Services.AddressService;
 	readonly #publicKeyService!: Services.PublicKeyService;
-	readonly #multiSignatureService!: Services.MultiSignatureService;
-	readonly #multiSignatureSigner!: IoC.Factory<MultiSignatureSigner>;
 	readonly #request: Request;
 	readonly #app: Application;
 
@@ -30,8 +29,6 @@ export class TransactionService extends Services.AbstractTransactionService {
 		this.#ledgerService = container.get(IoC.BindingType.LedgerService);
 		this.#addressService = container.get(IoC.BindingType.AddressService);
 		this.#publicKeyService = container.get(IoC.BindingType.PublicKeyService);
-		this.#multiSignatureService = container.get(IoC.BindingType.MultiSignatureService);
-		this.#multiSignatureSigner = container.factory(MultiSignatureSigner);
 		this.#transactionBuilder = container.factory(Transactions.BuilderFactory);
 		this.#app = container.get(BindingType.Application);
 
@@ -47,12 +44,7 @@ export class TransactionService extends Services.AbstractTransactionService {
 		);
 	}
 
-	/**
-	 * @inheritDoc
-	 *
-	 * @musig
-	 */
-	public override async transfer(input: Services.TransferInput): Promise<Contracts.SignedTransactionData> {
+	public override async #validateInput(input: Services.TransferInput): void {
 		if (!input.data.amount) {
 			throw new Error(
 				`[TransactionService#transfer] Expected amount to be defined but received ${typeof input.data.amount}`,
@@ -64,144 +56,58 @@ export class TransactionService extends Services.AbstractTransactionService {
 				`[TransactionService#transfer] Expected fee to be defined but received ${typeof input.fee}`,
 			);
 		}
+	}
 
-		return this.#createFromData("transfer", input, ({ transaction, data }) => {
-			transaction.recipientId(data.to);
+	public override async transfer(input: Services.TransferInput): Promise<Contracts.SignedTransactionData> {
+		applyCryptoConfiguration(this.#configCrypto);
+		this.#validateInput(input);
 
-			if (data.memo) {
-				transaction.vendorField(data.memo);
-			}
-		});
+		const transaction = this.#app.resolve(EvmCallBuilder)
+
+		const { address } = await this.#signerData(input)
+		const nonce = await this.#generateNonce(address, input)
+
+		transaction
+			.network(Number(this.#configCrypto.crypto.network.nethash))
+			.gasLimit(GasLimit.Transfer)
+			.recipientAddress(input.data.to)
+			.payload("")
+			.nonce(nonce)
+			.value(this.toSatoshi(input.data.amount).toString()) // revisit
+			.gasPrice(this.toSatoshi(input.fee).toNumber()) // revisit
+
+		return this.#buildTransaction(input, transaction)
+
 	}
 
 	public override async delegateRegistration(
 		input: Services.ValidatorRegistrationInput,
 	): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData(
-			"delegateRegistration",
-			input,
-			({ transaction, data }: { transaction: any; data: { validatorPublicKey: string } }) => {
-				transaction.publicKeyAsset(data.validatorPublicKey);
-			},
-		);
+		throw new Exceptions.NotImplemented(this.constructor.name, this.delegateRegistration.name);
 	}
 
-	/**
-	 * @inheritDoc
-	 *
-	 * @musig
-	 */
 	public override async vote(input: Services.VoteInput): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData(
-			"vote",
-			input,
-			({
-				transaction,
-				data,
-			}: {
-				transaction: any;
-				data: {
-					votes: {
-						id: string;
-						amount: BigNumber;
-					}[];
-					unvotes: {
-						id: string;
-						amount: BigNumber;
-					}[];
-				};
-			}) => {
-				if (Array.isArray(data.unvotes)) {
-					const unvotes: string[] = [];
-
-					for (const unvote of data.unvotes) {
-						unvotes.push(unvote.id);
-					}
-
-					transaction.unvotesAsset(unvotes);
-				}
-
-				if (Array.isArray(data.votes)) {
-					const votes: string[] = [];
-
-					for (const vote of data.votes) {
-						votes.push(vote.id);
-					}
-
-					transaction.votesAsset(votes);
-				}
-			},
-		);
+		throw new Exceptions.NotImplemented(this.constructor.name, this.vote.name);
 	}
 
-	/**
-	 * @inheritDoc
-	 *
-	 * @musig
-	 */
 	public override async multiPayment(input: Services.MultiPaymentInput): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData("multiPayment", input, ({ transaction, data }) => {
-			if (data.memo) {
-				transaction.vendorField(data.memo);
-			}
-		});
+		throw new Exceptions.NotImplemented(this.constructor.name, this.multiPayment.name);
 	}
 
-	public override async usernameRegistration(
-		input: Services.UsernameRegistrationInput,
-	): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData(
-			"usernameRegistration",
-			input,
-			({ transaction, data }: { transaction: any; data: { username: string } }) => {
-				transaction.usernameAsset(data.username);
-			},
-		);
+	public override async usernameRegistration(input: Services.UsernameRegistrationInput,): Promise<Contracts.SignedTransactionData> {
+		throw new Exceptions.NotImplemented(this.constructor.name, this.usernameRegistration.name);
 	}
 
-	public override async usernameResignation(
-		input: Services.UsernameResignationInput,
-	): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData("usernameResignation", input);
+	public override async usernameResignation(input: Services.UsernameResignationInput,): Promise<Contracts.SignedTransactionData> {
+		throw new Exceptions.NotImplemented(this.constructor.name, this.usernameResignation.name);
 	}
 
-	public override async delegateResignation(
-		input: Services.DelegateResignationInput,
-	): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData("delegateResignation", input);
+	public override async delegateResignation(input: Services.DelegateResignationInput,): Promise<Contracts.SignedTransactionData> {
+		throw new Exceptions.NotImplemented(this.constructor.name, this.delegateResignation.name);
 	}
 
-	public override async estimateExpiration(value?: string): Promise<string | undefined> {
-		const { data: blockchain } = await this.#request.get("blockchain");
-		const { data: configuration } = await this.#request.get("node/configuration");
-
-		return BigNumber.make(blockchain.block.height)
-			.plus((value ? Number(value) : 5) * configuration.constants.activeValidators)
-			.toString();
-	}
-
-	/**
-	 * @inheritDoc
-	 *
-	 * @musig
-	 */
-	public override async multiSignature(
-		input: Services.MultiSignatureInput,
-	): Promise<Contracts.SignedTransactionData> {
-		return this.#createFromData(
-			"multiSignature",
-			input,
-			({ transaction, data }: { transaction: any; data: any }) => {
-				if (data.senderPublicKey) {
-					transaction.senderPublicKey(data.senderPublicKey);
-				}
-
-				transaction.multiSignatureAsset({
-					min: data.min,
-					publicKeys: data.publicKeys,
-				});
-			},
-		);
+	public override async multiSignature(input: Services.MultiSignatureInput,): Promise<Contracts.SignedTransactionData> {
+		throw new Exceptions.NotImplemented(this.constructor.name, this.multiSignature.name);
 	}
 
 	async #signerData(input: Services.TransactionInputs): Promise<{ address?: string, publicKey?: string }> {
@@ -243,109 +149,17 @@ export class TransactionService extends Services.AbstractTransactionService {
 		return { address, publicKey }
 	}
 
-	async #createFromData(
-		type: string,
-		input: Services.TransactionInputs,
-		callback?: Function,
-	): Promise<Contracts.SignedTransactionData> {
-		applyCryptoConfiguration(this.#configCrypto);
-
-		const { address, publicKey } = await this.#signerData(input)
-		const transaction = this.#app.resolve(EvmCallBuilder)
-
-		// if (publicKey) {
-		// 	transaction.senderPublicKey(publicKey);
-		// }
-
-		if (input.nonce) {
-			transaction.nonce(input.nonce);
-		} else {
-			const wallet = await this.clientService.wallet({ type: "address", value: address! });
-
-			transaction.nonce(wallet.nonce().plus(1).toFixed(0));
+	async #generateNonce(address?: string, input?: Services.TransactionInputs): Promise<string> {
+		if (input?.nonce) {
+			return input.nonce
 		}
 
-		if (input.data && input.data.amount) {
-			transaction.value(this.toSatoshi(input.data.amount).toString());
-		}
+		const wallet = await this.clientService.wallet({ type: "address", value: address });
 
-		// if (input.data && Array.isArray(input.data.payments)) {
-		// 	for (const { amount, to } of input.data.payments) {
-		// 		transaction.addPayment(to, BigNumber.make(this.toSatoshi(amount)).toString());
-		// 	}
-		// }
+		return wallet.nonce().plus(1).toFixed(0);
+	}
 
-		if (input.fee) {
-			transaction.gasPrice(this.toSatoshi(input.fee).toNumber());
-		}
-
-		// try {
-		// 	if (input.data && input.data.expiration) {
-		// 		transaction.expiration(input.data.expiration);
-		// 	} else {
-		// 		let estimatedExpiration: string | undefined;
-		//
-		// 		if (
-		// 			input.signatory.actsWithMultiSignature() ||
-		// 			input.signatory.hasMultiSignature() ||
-		// 			type === "multiSignature"
-		// 		) {
-		// 			estimatedExpiration = await this.estimateExpiration("211");
-		// 		} else {
-		// 			estimatedExpiration = await this.estimateExpiration("5");
-		// 		}
-		//
-		// 		if (estimatedExpiration) {
-		// 			transaction.expiration(Number.parseInt(estimatedExpiration));
-		// 		}
-		// 	}
-		// } catch {
-		// If we fail to set the expiration we'll still continue.
-		// }
-
-		if (callback) {
-			callback({ data: input.data, transaction });
-		}
-
-		if (input.signatory.actsWithMultiSignature()) {
-			const serialized = await this.#app.resolve(Utils).toBytes(transaction.data);
-			const id = await this.#app.resolve(Utils).getId({ serialized } as MainsailContracts.Crypto.Transaction);
-
-			transaction.data.id = id.toString();
-			const transactionWithSignature = this.#multiSignatureSigner().sign(transaction, input.signatory.asset());
-
-			return this.dataTransferObjectService.signedTransaction(
-				transactionWithSignature.id!,
-				transactionWithSignature,
-			);
-		}
-
-		if (input.signatory.hasMultiSignature()) {
-			return await this.#addSignature(transaction, input.signatory.multiSignature()!, input.signatory);
-		}
-
-		if (type === "multiSignature") {
-			return await this.#addSignature(
-				transaction,
-				{
-					min: input.data.min,
-					publicKeys: input.data.publicKeys,
-				},
-				input.signatory,
-				senderPublicKey,
-			);
-		}
-
-		if (input.signatory.actsWithLedger()) {
-			transaction.data.signature = await this.#ledgerService.signTransaction(
-				input.signatory.signingKey(),
-				Transactions.Serializer.getBytes(transaction.data, {
-					excludeSignature: true,
-				}),
-			);
-
-			await this.#ledgerService.disconnect();
-		}
+	async #buildTransaction(input: Services.TransactionInputs, transaction: any): Promise<Contracts.SignedTransactionData> {
 
 		let signedTransactionBuilder;
 
@@ -379,31 +193,5 @@ export class TransactionService extends Services.AbstractTransactionService {
 		const signedTransaction = await signedTransactionBuilder?.build();
 
 		return this.dataTransferObjectService.signedTransaction(signedTransaction.id!, signedTransaction.data);
-	}
-
-	async #addSignature(
-		transaction,
-		multiSignature: Interfaces.IMultiSignatureAsset,
-		signatory: Signatories.Signatory,
-		senderPublicKey?: string,
-	): Promise<Contracts.SignedTransactionData> {
-		transaction.data.signatures = [];
-
-		if (senderPublicKey) {
-			transaction.senderPublicKey(senderPublicKey);
-		} else {
-			transaction.senderPublicKey(Identities.PublicKey.fromMultiSignatureAsset(multiSignature));
-		}
-
-		const struct = transaction.data;
-
-		const serialized = await this.#app.resolve(Utils).toBytes(struct);
-
-		const id = await this.#app.resolve(Utils).getId({ serialized } as MainsailContracts.Crypto.Transaction);
-
-		struct.id = id.toString();
-		struct.multiSignature = multiSignature;
-
-		return this.#multiSignatureService.addSignature(struct, signatory);
 	}
 }
