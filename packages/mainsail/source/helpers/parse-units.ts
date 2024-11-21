45a7a3fcaaa46a88496e729f060ebc80d42f5d3e
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-11-21 14:57:31 -0500

    refactor: enable tests on mainsail (#119)

--- packages/mainsail/source/helpers/parse-units.ts
+++ packages/mainsail/source/helpers/parse-units.ts
@@ -1,15 +1,15 @@
-import { BigNumber } from "@mainsail/kernel/distribution/utils";
+import { BigNumber } from "@ardenthq/sdk-helpers";
 
 import { ARK_MULTIPLIER, GWEI_MULTIPLIER, WEI_MULTIPLIER } from "../crypto/constants";
 
 export const parseUnits = (value: number | string, unit = "ark"): string => {
 	switch (unit.toLowerCase()) {
 		case "wei":
-			return BigNumber.make(value).times(WEI_MULTIPLIER).toString();
+			return BigNumber.make(value).times(WEI_MULTIPLIER).valueOf();
 		case "gwei":
-			return BigNumber.make(value).times(GWEI_MULTIPLIER).toString();
+			return BigNumber.make(value).times(GWEI_MULTIPLIER).valueOf();
 		case "ark":
-			return BigNumber.make(value).times(ARK_MULTIPLIER).toString();
+			return BigNumber.make(value).times(ARK_MULTIPLIER).valueOf();
 		default:
 			throw new Error(`Unsupported unit: ${unit}. Supported units are 'wei', 'gwei', and 'ark'.`);
 	}
