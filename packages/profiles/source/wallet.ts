ec1a218aed097814cfcef17782e4651b8779eca7
Shahin Safaraliyev <shahin@ardenthq.com>
2024-11-22 20:23:27 +0400

    fix: extract decimals from network config

--- packages/profiles/source/wallet.ts
+++ packages/profiles/source/wallet.ts
@@ -668,9 +668,9 @@ export class Wallet implements IReadWriteWallet {
 	#decimals(): number {
 		try {
 			const manifest: Networks.NetworkManifest = this.coin().manifest().get<object>("networks")[this.networkId()];
-			return manifest.currency.decimals;
+			return manifest.currency.decimals ?? 18;
 		} catch {
-			return 8;
+			return 18;
 		}
 	}
 }
