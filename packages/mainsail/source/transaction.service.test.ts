1be069d666d687a5826e73314fb3d8f7da9d236a
ItsANameToo <itsanametoo@protonmail.com>
2024-11-22 19:43:37 +0100

    wip

--- packages/mainsail/source/transaction.service.test.ts
+++ packages/mainsail/source/transaction.service.test.ts
@@ -14,7 +14,18 @@ import { Transactions } from "./crypto/index.js";
 import { WalletData } from "./wallet.dto.js";
 import { createService } from "../test/mocking";
 import { describe } from "@ardenthq/sdk-test";
-import { identity } from "../test/fixtures/identity";
+
+// TODO: update for Mainsail and move to fixture file
+const identity = {
+	// Standard
+	privateKey: "e2511a6022953eb399fbd48f84619c04c894f735aee107b02a7690075ae67617",
+	publicKey: "030fde54605c5d53436217a2849d276376d0b0f12c71219cd62b0a4539e1e75acd",
+	address: "D6i8P5N44rFto6M6RALyUXLLs7Q1A1WREW",
+	wif: "SHA89yQdW3bLFYyCvEBpn7ngYNR8TEojGCC1uAJjT5esJPm1NiG3",
+	mnemonic: "bomb open frame quit success evolve gain donate prison very rent later",
+	multiSignatureAddress: "DMS861mLRrtH47QUMVif3C2rBCAdHbmwsi",
+	multiSignaturePublicKey: "0279f05076556da7173610a7676399c3620276ebbf8c67552ad3b1f26ec7627794",
+};
 
 describe("TransactionService", async ({ assert, beforeAll, nock, it, loader }) => {
 	beforeAll(async (context) => {
