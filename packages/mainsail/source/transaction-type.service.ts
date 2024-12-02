9435aa8e1a0802901c1aac5d6f916720b7b12228
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2024-12-02 16:23:47 +0100

    Update packages/mainsail/source/transaction-type.service.ts
    Co-authored-by: ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>

--- packages/mainsail/source/transaction-type.service.ts
+++ packages/mainsail/source/transaction-type.service.ts
@@ -36,7 +36,7 @@ export class TransactionTypeService {
 		// When signing transaction, mainsail removes the 0x prefix form the data payload forcing these tx type checks to always be false
 		// as the TransactionTypes from mainsail consensus are always prefixed with 0x.
 		// @TODO: Revisit these checks. See relevant issue https://app.clickup.com/t/86dvawadc
-		return data.data.includes(TransactionTypes.Vote.slice(2));
+		return data.data.includes(TransactionTypes.Vote.slice(2)); // remove `0x` prefix from api response
 	}
 
 	public static isUnvote(data: TransactionData): boolean {
