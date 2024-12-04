02e499816313e227a517274b71b429ec196e6474
goga-m <goga-m@users.noreply.github.com>
2024-12-04 14:50:15 +0000

    style: resolve style guide violations

--- packages/sdk/source/confirmed-transaction.dto.ts
+++ packages/sdk/source/confirmed-transaction.dto.ts
@@ -366,5 +366,4 @@ export abstract class AbstractConfirmedTransactionData implements ConfirmedTrans
 	public isSuccess(): boolean {
 		throw new Exceptions.NotImplemented(this.constructor.name, this.isSuccess.name);
 	}
-
 }
