1bf590d34200f1ddb333afa061ee9bce9da7f8a6
Shahin Safaraliyev <shahin@ardenthq.com>
2025-01-10 13:10:44 +0400

    fix: transaction fee formatting

--- packages/mainsail/source/signed-transaction.dto.ts
+++ packages/mainsail/source/signed-transaction.dto.ts
@@ -44,7 +44,7 @@ export class SignedTransactionData
 
 	public override fee(): BigNumber {
 		const gasPrice = this.bigNumberService.make(this.signedData.gasPrice);
-		return parseUnits(gasPrice.times(this.signedData.gasLimit).toNumber(), "gwei");
+		return this.bigNumberService.make(parseUnits(gasPrice.times(this.signedData.gasLimit).toNumber(), "gwei"));
 	}
 
 	public override memo(): string | undefined {
