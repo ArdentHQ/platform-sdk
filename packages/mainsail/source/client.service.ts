5251305be6c1523474a853171506fb1df12e270c
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2025-01-15 10:59:30 +0100

    Update packages/mainsail/source/client.service.ts

--- packages/mainsail/source/client.service.ts
+++ packages/mainsail/source/client.service.ts
@@ -234,8 +234,8 @@ export class ClientService extends Services.AbstractClientService {
 			for (const type of body.types) {
 				const datum = transactionTypeMap[type];
 
+				// a transfer is an empty string, so explicitly check for undefined
 				if (datum !== undefined) {
-					// a transfer is an empty string, so explicitly check for undefined
 					data.push(trimHexPrefix(datum));
 				}
 			}
