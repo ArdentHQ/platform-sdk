b0807f48d4a2fae41d4b21e5d1b29ca2fe2295b3
ItsANameToo <itsanametoo@protonmail.com>
2025-01-16 17:06:36 +0100

    chore: use npm instead of pnpm for publish

--- scripts/publish-experimental.sh
+++ scripts/publish-experimental.sh
@@ -3,6 +3,6 @@
 for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
     cd $dir
     echo $PWD
-    NODE_AUTH_TOKEN=$1 pnpm publish --access=public --publish-branch=feat/mainsail-evm --tag=experimental
+    NODE_AUTH_TOKEN=$1 npm publish --access=public --publish-branch=feat/mainsail-evm --tag=experimental
     cd ../..
 done
