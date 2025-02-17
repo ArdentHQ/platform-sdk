4d486eb79ec77bd871ee64169f003349955130bb
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-17 09:45:38 -0500

    fix: identity

--- packages/ark/test/fixtures/identity.ts
+++ packages/ark/test/fixtures/identity.ts
@@ -12,6 +12,6 @@ export const identity = {
 	multiSignatureAddress: "DMS861mLRrtH47QUMVif3C2rBCAdHbmwsi",
 	multiSignaturePublicKey: "0279f05076556da7173610a7676399c3620276ebbf8c67552ad3b1f26ec7627794",
 	privateKey: "e2511a6022953eb399fbd48f84619c04c894f735aee107b02a7690075ae67617",
-	publicKey: "e2511a6022953eb399fbd48f84619c04c894f735aee107b02a7690075ae6",
+	publicKey: "030fde54605c5d53436217a2849d276376d0b0f12c71219cd62b0a4539e1e75acd",
 	wif: "SHA89yQdW3bLFYyCvEBpn7ngYNR8TEojGCC1uAJjT5esJPm1NiG3",
 };
