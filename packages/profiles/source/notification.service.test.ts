a231752a06071c1f73af2b9b9007b8fa58d3423b
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-17 08:48:27 -0500

    refactor: notification service

--- packages/profiles/source/notification.service.test.ts
+++ packages/profiles/source/notification.service.test.ts
@@ -31,7 +31,7 @@ describeWithContext(
 				.get("/api/wallets", {})
 				.query({ limit: 1, nonce: 0 })
 				.reply(200, {})
-				.get("/api/wallets/D6i8P5N44rFto6M6RALyUXLLs7Q1A1WREW")
+				.get("/api/wallets/0xA5cc0BfEB09742C5e4C610f2EBaaB82Eb142Ca10")
 				.reply(200, loader.json("test/fixtures/client/wallet.json"))
 				.get("/api/transactions")
 				.query(true)
