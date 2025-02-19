79cfec0e69522a2d93e4358563cb9769fd1dfe19
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-19 10:53:05 -0500

    test: wif service

---
+++ packages/mainsail/source/wif.service.test.ts
@@ -0,0 +1,29 @@
+import { describe } from "@ardenthq/sdk-test";
+
+import { identity, identityWithSecret } from '../test/fixtures/identity';
+import { createService } from "../test/mocking";
+import { WIFService } from "./wif.service.js";
+
+describe("WIFService", ({ assert, beforeEach, it, nock, loader }) => {
+	beforeEach(async (context) => {
+		context.subject = await createService(WIFService);
+	});
+
+	it("fromMnemonic", async (context) => {
+		const result = await context.subject.fromMnemonic(identity.mnemonic);
+
+		assert.equal(result, { wif: identity.wif });
+	});
+
+	it("fromSecret", async (context) => {
+		const result = await context.subject.fromSecret(identityWithSecret.secret);
+
+		assert.equal(result, { wif: identityWithSecret.wif });
+	});
+
+	it("fromPrivateKey", async (context) => {
+		const result = await context.subject.fromPrivateKey(identity.privateKey);
+
+		assert.equal(result, { wif: identity.wif });
+	});
+});
