907f61fd0bc7c0fbd1b2eb192aacec41a84b783e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-20 05:16:43 -0500

    refactor: crypto domain utils tests

---
+++ packages/mainsail/source/crypto/utils.test.ts
@@ -0,0 +1,36 @@
+import { describe } from "@ardenthq/sdk-test";
+
+import { configManager } from "./managers";
+import { isSupportedTransactionVersion, maxVendorFieldLength } from './utils';
+
+describe("CryptoUtils", ({ assert, it }) => {
+    describe("maxVendorFieldLength", () => {
+        it("should return the vendorFieldLength for a given height", () => {
+            configManager.getMilestone = (height?: number) =>
+                ({ vendorFieldLength: 64 } as any)
+                ;
+
+            const result = maxVendorFieldLength(100);
+            assert.equal(result, 64);
+        });
+
+        it("should return the vendorFieldLength when no height is provided", () => {
+            configManager.getMilestone = () => ({ vendorFieldLength: 128 } as any);
+
+            const result = maxVendorFieldLength();
+            assert.equal(result, 128);
+        });
+    });
+
+    describe("isSupportedTransactionVersion", () => {
+        it("should return true when version is 1", () => {
+            const result = isSupportedTransactionVersion(1);
+            assert.true(result);
+        });
+
+        it("should return false when version is not 1", () => {
+            assert.false(isSupportedTransactionVersion(2));
+            assert.false(isSupportedTransactionVersion(3));
+        });
+    });
+});
