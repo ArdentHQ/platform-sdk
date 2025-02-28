914f8c4ef4a36bf6bc75096f910dcc7501a582f5
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-02-28 14:31:07 +0100

    chore: remove `hw-app-eth` dependency (#187)

--- packages/mainsail/source/ledger.service.ts
+++ packages/mainsail/source/ledger.service.ts
@@ -1,7 +1,6 @@
 import { Buffer } from "buffer";
 import { Collections, Contracts, IoC, Services } from "@ardenthq/sdk";
 import { BIP44, HDKey } from "@ardenthq/sdk-cryptography";
-import Eth from "@ledgerhq/hw-app-eth";
 import { Exceptions } from "@mainsail/contracts";
 import { chunk, createRange, formatLedgerDerivationPath } from "./ledger.service.helpers.js";
 
@@ -9,7 +8,7 @@ export class LedgerService extends Services.AbstractLedgerService {
 	readonly #clientService!: Services.ClientService;
 	readonly #addressService!: Services.AddressService;
 	#ledger!: Services.LedgerTransport;
-	#transport!: InstanceType<typeof Eth>;
+	#transport!: unknown;
 
 	public constructor(container: IoC.IContainer) {
 		super(container);
@@ -24,8 +23,7 @@ export class LedgerService extends Services.AbstractLedgerService {
 
 	public override async connect(): Promise<void> {
 		this.#ledger = await this.ledgerTransportFactory();
-		this.#transport = new Eth(this.#ledger);
-		console.log("[connect]", { transport: this.#transport });
+		throw new Exceptions.NotImplemented(this.constructor.name, this.connect.name);
 	}
 
 	public override async disconnect(): Promise<void> {
@@ -39,9 +37,7 @@ export class LedgerService extends Services.AbstractLedgerService {
 	}
 
 	public override async getPublicKey(path: string): Promise<string> {
-		const result = await this.#transport.getAddress(path);
-		console.log("[getPublicKey]", result);
-		return result.publicKey;
+		throw new Exceptions.NotImplemented(this.constructor.name, this.getPublicKey.name);
 	}
 
 	public override async getExtendedPublicKey(path: string): Promise<string> {
