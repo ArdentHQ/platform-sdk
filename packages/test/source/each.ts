081fc34caba81e6fafc4857e937a5c1b9be785a7
ItsANameToo <itsanametoo@protonmail.com>
2025-03-28 11:18:52 +0100

    wip

--- packages/test/source/each.ts
+++ packages/test/source/each.ts
@@ -1,8 +1,8 @@
-import { format as concordance } from "concordance";
-import { format } from "string-kit";
 import { Callback, Context, Test, test } from "uvu";
 
-export const formatName = (name: string, dataset: unknown): string => format(name, concordance(dataset));
+import { format as concordance } from "concordance";
+
+export const formatName = (name: string, dataset: unknown): string => `${name}, ${concordance(dataset)}`;
 
 export const each = (test: Test) => (name: string, callback: Callback<any>, datasets: unknown[]) => {
 	for (const dataset of datasets) {
