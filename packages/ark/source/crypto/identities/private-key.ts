425d235d69bc70e3b07e540f4bfdf16452b4f76c
Shahin Safaraliyev <shahin@ardenthq.com>
2026-02-02 16:16:55 +0400

    feat: support address creation with BIP44

--- packages/ark/source/crypto/identities/private-key.ts
+++ packages/ark/source/crypto/identities/private-key.ts
@@ -3,7 +3,7 @@ import { Keys } from "./keys.js";
 
 export class PrivateKey {
 	public static fromPassphrase(passphrase: string): string {
-		return path ? Keys.fromBip44Mnemonic(passphrase, path).privateKey : Keys.fromPassphrase(passphrase).privateKey;
+		return Keys.fromPassphrase(passphrase).privateKey;
 	}
 
 	public static fromBip44Mnemonic(passphrase: string, path: string): string {
